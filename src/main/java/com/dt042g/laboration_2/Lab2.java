4ac1aea5d36322104a8897423ac5c49124ae757b
Erik Ström <Erik.Strom@miun.se>
2023-01-30 14:43:34 +0100

    initial commit

---
+++ src/main/java/com/dt042g/laboration_2/Lab2.java
@@ -0,0 +1,7 @@
+package com.dt042g.laboration_2;
+
+public class Lab2 {
+    static public void main(String... args) {
+        System.out.println("Laboration 2");
+    }
+}
---
+++ src/test/java/com/dt042g/laboration_2/Lab2Test.java
@@ -0,0 +1,13 @@
+package com.dt042g.laboration_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Lab2Test {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
