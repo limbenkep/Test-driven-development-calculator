fbab6ca09cd0c1ee6defae70021da164e3d6bd54
Honorine Lima <holi1900@student.miun.se>
2023-02-12 07:52:43 +0100

    Add test and method to perform subtraction
    Adds test for performing subtration in Lab2Test and implement method in Lab3

--- src/main/java/com/dt042g/laboration_2/Lab2.java
+++ src/main/java/com/dt042g/laboration_2/Lab2.java
@@ -1,7 +1,39 @@
 package com.dt042g.laboration_2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lab2 {
     static public void main(String... args) {
         System.out.println("Laboration 2");
+    }
+
+    /**
+     * Receives expression as a string which is expected to contain only subtraction
+     * Since subtraction is the last sign to be computed.
+     * splits expression at '-' sign and
+     * Performs all subtraction in the expression and returns the answer as an integer.
+     * @param expression Mathematical expression to be computed
+     * @return result of the comutation as an int
+     */
+    public static int performAllSubtractionInInput(String expression) {
+        if(!expression.contains("-")){
+            return Integer.parseInt(expression);
+        }
+        boolean startWithNeg= expression.charAt(0) == '-';
+        List<String> list = Arrays.asList(expression.split("-"));
+
+        int result;
+        if(startWithNeg){
+            result = -Integer.parseInt(list.get(1));
+        }else{
+            result =Integer.parseInt(list.get(0)) - Integer.parseInt(list.get(1));
+        }
+        if(list.size()>2){
+            for(int i=2; i<list.size(); i++){
+                result -= Integer.parseInt(list.get(i));
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/dt042g/laboration_2/Lab2Test.java
+++ src/test/java/com/dt042g/laboration_2/Lab2Test.java
@@ -1,13 +1,41 @@
 package com.dt042g.laboration_2;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+/**
+ * @Author: Honorine Lima
+ * username:holi1900
+ * Course: DT042G
+ * version: 1
+ *
+ *
+ * This class is a calculator class that takes a mathematical expression in the from of a string,
+ * checks if it is a valid mathematical expression and
+ * if it is valid, it computes the expression according to order of operator precedence
+ *  that is  brackets, exponent, division, multiplacation, addition, subtraction and returns the answer
+ * if the expression is not a valid expression, it returns the expression without white spaces
+ *
+ */
 public class Lab2Test {
+    /**
+     * test function performAllSubtractionInInput that computes all subtractions in the input expression
+     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testPerformAllSubtractionInInput(){
+        assertEquals(1, Lab2.performAllSubtractionInInput("5-4"));
+        assertEquals(-12, Lab2.performAllSubtractionInInput("-3-5-4"));
     }
 }
